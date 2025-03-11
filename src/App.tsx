08c25b204cb4cddd14c52b5640168e7b34c60577
Amandeep <amanr5413@gmail.com>
2025-03-11 22:57:00 +0530

    [Aman] Add : Adding BookDetails Cart Myorder WishList

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,10 @@ import Register from './pages/Register'
 import Login from './pages/Login'
 import Home from './pages/Home'
 import ForgotPassword from './pages/ForgetPassword'
-
+import BookPage from './pages/BookPage'
+import MyOrder from './pages/MyOrder'
+import WishList from './pages/WishList'
+import Cart from './pages/Cart'
 const App = () => {
   return (
     <div>
@@ -14,6 +17,10 @@ const App = () => {
         <Route path='/register' element={<Register/>} />
         <Route path='/home' element={<Home/>} />
         <Route path='/forgotPassword' element={<ForgotPassword />}/>
+        <Route path='/home/:id' element={<BookPage />}/>
+        <Route path='myOrder' element={<MyOrder/>}/>
+        <Route path='/wishlist' element={<WishList/>}/>
+        <Route path='/cart' element={<Cart/>}/>
       </Routes>
     </div>
   )
