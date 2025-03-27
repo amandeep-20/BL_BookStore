691cea2881add626d18689b43c565b9b210ee5ac
Amandeep <amanr5413@gmail.com>
2025-03-27 23:09:50 +0530

    [Aman] Add : setup of jest and rtl in project

--- jest.config.ts
+++ jest.config.ts
@@ -1,10 +1,24 @@
-export default {
-    testEnvironment: 'jsdom',
-    setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
-    transform: {
-      '^.+\\.(ts|tsx)$': 'ts-jest',
-    },
-    moduleNameMapper: {
-      '\\.(css|scss|less)$': 'identity-obj-proxy',
-    },
-  };
+import type { Config } from 'jest';
+
+const config: Config = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  },
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  collectCoverage: false,
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+    '!src/**/*.test.{ts,tsx}',
+    '!src/**/index.{ts,tsx}',
+    '!src/**/*.{types,d}.{ts,tsx}',
+  ],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov', 'clover'],
+};
+
+export default config;
